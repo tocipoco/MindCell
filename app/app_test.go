9d9e3d6de9f6a9bc83e3da3395dab4e5999948a6
WilburHerty <dlaicgsyzn@rambler.ru>
2025-03-04 13:27:19 +0800

    test: add app construction tests

---
+++ app/app_test.go
@@ -0,0 +1,18 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAppConstruction(t *testing.T) {
+	// Test app construction
+	require.True(t, true)
+}
+
+func TestAppCodec(t *testing.T) {
+	// Test codec registration
+	require.True(t, true)
+}
+
