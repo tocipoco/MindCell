d352ae182cbc43fb3f86a4795dde08cd24d70c1d
WilburHerty <dlaicgsyzn@rambler.ru>
2025-03-27 15:42:31 +0800

    feat: add ShardAllocator event constants

---
+++ x/shardallocator/types/events.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	EventTypeRegisterNode    = "register_node"
+	EventTypeAssignShard     = "assign_shard"
+	EventTypeReplaceShard    = "replace_shard"
+	EventTypeUpdateReputation = "update_reputation"
+
+	AttributeKeyNodeAddress = "node_address"
+	AttributeKeyShardID     = "shard_id"
+	AttributeKeyReputation  = "reputation"
+)
+
