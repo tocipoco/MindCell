9bb557612a49d226493ef1e37677cde6cd49e5d1
HenryGarcia-50z <tdlfnpwwlf@rambler.ru>
2025-03-29 10:19:54 +0800

    feat: add InferenceGateway event constants

---
+++ x/inferencegateway/types/events.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	EventTypeInferenceSubmitted  = "inference_submitted"
+	EventTypeProofVerified       = "proof_verified"
+	EventTypeInferenceCompleted  = "inference_completed"
+
+	AttributeKeyRequestID = "request_id"
+	AttributeKeyRequester = "requester"
+	AttributeKeyModelID   = "model_id"
+	AttributeKeyProofHash = "proof_hash"
+)
+
