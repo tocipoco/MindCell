5338afd3fe1744f83a8ed2bffc2aeff9dc6028d1
TheodoreRodriguez-yr1 <ppwopowtki@rambler.ru>
2025-03-31 14:55:23 +0800

    feat: add Billing and Reward event constants

---
+++ x/billing/types/events.go
@@ -0,0 +1,12 @@
+package types
+
+const (
+	EventTypePaymentProcessed = "payment_processed"
+	EventTypeBillingSettled   = "billing_settled"
+	EventTypeFeeConfigUpdated = "fee_config_updated"
+
+	AttributeKeyTotalFee  = "total_fee"
+	AttributeKeyRequestID = "request_id"
+	AttributeKeyBaseFee   = "base_fee"
+)
+
---
+++ x/reward/types/events.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	EventTypeRewardDistributed = "reward_distributed"
+	EventTypeRewardClaimed     = "reward_claimed"
+	EventTypePoolFunded        = "pool_funded"
+
+	AttributeKeyAmount      = "amount"
+	AttributeKeyNodeAddress = "node_address"
+	AttributeKeyClaimer     = "claimer"
+	AttributeKeyNewTotal    = "new_total"
+)
+
