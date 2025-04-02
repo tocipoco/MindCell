29921f6b1611adf4dbc248f3432a2907ad707e75
tocipoco <tocipoci424@gmail.com>
2025-04-02 09:37:48 +0800

    feat: add Slashing and Token event constants

---
+++ x/slashing/types/events.go
@@ -0,0 +1,12 @@
+package types
+
+const (
+	EventTypeNodeSlashed      = "node_slashed"
+	EventTypeParamsUpdated    = "slashing_params_updated"
+
+	AttributeKeyNodeAddress = "node_address"
+	AttributeKeySlashType   = "slash_type"
+	AttributeKeyAmount      = "amount"
+	AttributeKeyReason      = "reason"
+)
+
---
+++ x/token/types/events.go
@@ -0,0 +1,12 @@
+package types
+
+const (
+	EventTypeTokensMinted = "tokens_minted"
+	EventTypeTokensBurned = "tokens_burned"
+
+	AttributeKeyRecipient = "recipient"
+	AttributeKeyBurner    = "burner"
+	AttributeKeyAmount    = "amount"
+	AttributeKeyNewSupply = "new_supply"
+)
+
